57e554367c40b8ccc6c75e3e741e856a10d81aaa
Glacycy <gomez.angeline123@hotmail.fr>
2025-07-25 17:24:50 +0200

    tp02

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,10 +1,17 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            String art = FigletFont.convertOneLine("Hello World!");
+            System.out.println(art);
+        } catch (Exception e) {
+            System.err.println("Erreur: " + e.getMessage());
+        }
     }
 }
